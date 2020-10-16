b79d958b99ee8d70d0f711f1b8b2ad4455185d59
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-10-16 14:00:46 +0300

    Add connection to DB

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const testPage = require("./routes/index");
+const mongoose = require("mongoose");
+
 
 const app = express();
 
@@ -10,16 +13,24 @@ const PORT = process.env.PORT || 3000;
 /////////////////////////////////////////////////////////
 app.use("/", testPage);
 
-
 /////////////////////////////////////////////////////////
 // Create server
 /////////////////////////////////////////////////////////
 const startServer = async () => {
   try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gehfl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      }
+    );
     app.listen(PORT, () => {
-        console.log("First Log");
-      });
+      console.log("First Log");
+    });
   } catch (e) {
+    console.log(e);
     throw new Exception(e);
   }
 };
