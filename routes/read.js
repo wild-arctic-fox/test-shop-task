6250cf1298f3555b02c827626f0b48c0454bf4f3
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-10-16 16:30:23 +0300

    Add router for reading products data

--- routes/read.js
+++ routes/read.js
@@ -1,19 +1,36 @@
 const { Router } = require("express");
 const ProviderModel = require("../models/providerModel");
+const ProductModel = require("../models/productModel");
 
 /////////////////////////////////////////////////////////
-// Router for displaying providers
+// Router for displaying providers and products
 /////////////////////////////////////////////////////////
 const router = Router();
 
 /////////////////////////////////////////////////////////
 // Router for displaying providers by ID
-router.get("/:id", async (req, res) => {
+router.get("/provider/:id", async (req, res) => {
   try {
     const data = await ProviderModel.findById(req.params.id);
     res.setHeader("Content-Type", "application/json");
     if (!data) {
       res.send(JSON.stringify({ error: "this provider is not found" }));
+    } else {
+      res.send(JSON.stringify(data));
+    }
+  } catch (e) {
+    throw new Error();
+  }
+});
+
+/////////////////////////////////////////////////////////
+// Router for displaying product by ID
+router.get("/product/:id", async (req, res) => {
+  try {
+    const data = await ProductModel.findById(req.params.id).lean();
+    res.setHeader("Content-Type", "application/json");
+    if (!data) {
+      res.send(JSON.stringify({ error: "this product is not found" }));
     } else {
       res.send(JSON.stringify(data));
     }
