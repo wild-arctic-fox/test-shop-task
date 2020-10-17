47f1a0107948417ae583ae979920b561111477ff
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-10-17 19:24:24 +0300

    Change returned status code

--- routes/create.js
+++ routes/create.js
@@ -11,25 +11,26 @@ const router = Router();
 /////////////////////////////////////////////////////////
 // Receive post data
 router.post("/provider", providerDataValidator, async (req, res) => {
+  // validate data
   const { name, email, phone } = req.body;
   const result = validationResult(req);
   if (!result.isEmpty()) {
     const { errors } = result;
-    res.status(500).send(JSON.stringify({ error: errors[0].msg }));
-    return;
+    return res.status(400).send(JSON.stringify({ message: errors[0].msg }));
   }
+
+  // try to save data
   const providerModel = new ProviderModel({ name, email, phone });
   try {
     await providerModel.save();
     res.setHeader("Content-Type", "application/json");
     if (!providerModel) {
-      return res.status(400).send({ message: "Bad request." });
+      return res.status(500).send({ message: "Unable create provider" });
     } else {
-      return res.status(200).send({ message: "Provider created." });
+      return res.status(200).send({ message: "Provider created" });
     }
   } catch (e) {
-    console.log(e);
-    throw new Exception(e);
+    return res.status(500).send({ message: "Internal server error" });
   }
 });
 
