7903e3ef103d81a6e153497bb7465f31322b8f5b
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-10-17 20:34:54 +0300

    Remove unused import

--- routes/delete.js
+++ routes/delete.js
@@ -1,5 +1,4 @@
 const { Router } = require("express");
-const ProviderModel = require("../models/providerModel");
 const { deleteProvider } = require("../db/deleteOperation");
 
 /////////////////////////////////////////////////////////
