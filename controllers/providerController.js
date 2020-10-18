7d517037c6558e01a292469fbd5687b8fccc248c
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-10-18 13:46:29 +0300

    Add controller for creating operation

--- controllers/providerController.js
+++ controllers/providerController.js
@@ -1,5 +1,7 @@
 const { readProviderData } = require("../db/readOperation");
 const { deleteProvider } = require("../db/deleteOperation");
+const { validationResult } = require("express-validator");
+const { createProvider } = require("../db/createOperation");
 
 const get = async (req, res) => {
   try {
@@ -30,4 +32,28 @@ const remove = async (req, res) => {
   }
 };
 
-module.exports = { get, remove };
+const create = async (req, res) => {
+  try {
+    // validate data
+    const { name, email, phone } = req.body;
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      const { errors } = result;
+      return res.status(400).send(JSON.stringify({ message: errors[0].msg }));
+    }
+
+    // try to save data
+    const provider = { name, email, phone };
+    const providerModel = await createProvider(provider);
+    res.setHeader("Content-Type", "application/json");
+    if (!providerModel) {
+      return res.status(500).send({ message: "Unable create provider" });
+    } else {
+      return res.status(200).send({ message: "Provider created" });
+    }
+  } catch (e) {
+    return res.status(500).send({ message: "Internal server error" });
+  }
+};
+
+module.exports = { get, remove, create };
--- routes/create.js
+++ routes/create.js
@@ -1,7 +1,6 @@
 const { Router } = require("express");
 const { providerDataValidator } = require("../helpers/utils/validators");
-const { validationResult } = require("express-validator");
-const { createProvider } = require("../db/createOperation");
+const { create: createProvider } = require("../controllers/providerController");
 
 /////////////////////////////////////////////////////////
 // Router for adding a new Providers and Products
@@ -9,30 +8,7 @@ const { createProvider } = require("../db/createOperation");
 const router = Router();
 
 /////////////////////////////////////////////////////////
-// Receive post data
-router.post("/provider", providerDataValidator, async (req, res) => {
-  try {
-
-    // validate data
-    const { name, email, phone } = req.body;
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
-      const { errors } = result;
-      return res.status(400).send(JSON.stringify({ message: errors[0].msg }));
-    }
-
-    // try to save data
-    const provider = { name, email, phone };
-    const providerModel = await createProvider(provider);
-    res.setHeader("Content-Type", "application/json");
-    if (!providerModel) {
-      return res.status(500).send({ message: "Unable create provider" });
-    } else {
-      return res.status(200).send({ message: "Provider created" });
-    }
-  } catch (e) {
-    return res.status(500).send({ message: "Internal server error" });
-  }
-});
+// Create provider
+router.post("/provider", providerDataValidator, createProvider);
 
 module.exports = router;
